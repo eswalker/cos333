ef6a25e4ea151fff40d84cb96c3d3cc6daf4c987
= <eswalker@princeton.edu>
2014-03-25 14:02:58 -0400

    add practices model

--- rowsite/row/models.py
+++ rowsite/row/models.py
@@ -7,6 +7,9 @@ class Athlete(models.Model):
     year = models.CharField(max_length=20)
     height = models.CharField(max_length=20)
     
+class Practice(models.Model):
+    datetime = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=20)
 
 class Result(models.Model):
     datetime = models.DateTimeField(auto_now=True)
@@ -14,6 +17,9 @@ class Result(models.Model):
     time = models.IntegerField()
     type = models.CharField(max_length=20)
     athlete = models.ForeignKey(Athlete)
+    practice = models.ForeignKey(Practice)
+
+
 
 class Weight(models.Model):
     datetime = models.DateTimeField(auto_now=True)
