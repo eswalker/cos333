786d58f260dd44c1a5f503958168a2e3c16059ad
sfjordan <samuelfjordan@gmail.com>
2014-03-27 11:26:36 -0400

    registering models

--- rowsite/row/admin.py
+++ rowsite/row/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from row.models import Athlete, Practice, Result, Weight
 
 # Register your models here.
+admin.site.register(Athlete)
+admin.site.register(Practice)
+admin.site.register(Result)
+admin.site.register(Weight)
