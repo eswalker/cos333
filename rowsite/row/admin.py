7c321e41247f4e17b6210467df8535d338e0b923
sfjordan <samuelfjordan@gmail.com>
2014-03-28 13:09:00 -0400

    admin site

--- rowsite/row/admin.py
+++ rowsite/row/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from row.models import Athlete, Practice, Result, Weight
 
 class AthleteAdmin(admin.ModelAdmin):
-	fields = ['name','year','height','side']
+	fields = ['name','year','side','height']
+
+class ResultAdmin(admin.ModelAdmin):
+	fieldsets = [
+		('Date and Distance', {'fields':['datetime','distance']}),
+		('More information', {'fields':['time','type','athlete','practice'], 'classes':['collapse']}),
+	]
+
 
 # Register your models here.
 admin.site.register(Athlete, AthleteAdmin)
