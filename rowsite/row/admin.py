270f4e8ed71cf307679a32cb8173ee04f0c9b286
sfjordan <samuelfjordan@gmail.com>
2014-03-28 12:40:44 -0400

    change admin site

--- rowsite/row/admin.py
+++ rowsite/row/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from row.models import Athlete, Practice, Result, Weight
 
+class AthleteAdmin(admin.ModelAdmin):
+	fields = ['name','year','height','side']
+
 # Register your models here.
-admin.site.register(Athlete)
+admin.site.register(Athlete, AthleteAdmin)
 admin.site.register(Practice)
 admin.site.register(Result)
 admin.site.register(Weight)
