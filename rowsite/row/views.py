ee9f2cef0f0ecc5b361145d2ee56248cfecf97f9
= <eswalker@princeton.edu>
2014-03-29 15:05:26 -0400

    weights to athlete details

--- rowsite/row/views.py
+++ rowsite/row/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 
-from row.models import Athlete
+from row.models import Athlete, Weight
 
 # Lists athletes in a roster
 def athlete_index(request):
@@ -13,9 +13,15 @@ def athlete_index(request):
 # Shows athlete details for one athlete
 def athlete_detail(request, athleteId):
     athlete = get_object_or_404(Athlete, pk=athleteId)
-    return render(request, 'row/athlete/details.html', {'athlete':athlete})
+    weights = Weight.objects.get(athlete=athleteId)
+    return render(request, 'row/athlete/details.html', {'athlete':athlete, 'weights':weights})
+
 
 # Creates a new athlete
 def athlete_new(request):
     return render(request, 'row/athlete/new.html')
+
+
+	
+
 
