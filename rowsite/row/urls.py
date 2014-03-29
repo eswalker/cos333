8e085940568eaad40c736c81ffba4b77125e0925
Brian Rosenfeld <bmr0802@gmail.com>
2014-03-29 14:57:23 -0400

    resolved links between urls and newly named methods in views

--- rowsite/row/urls.py
+++ rowsite/row/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from row import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^athlete/(?P<athleteId>\d+)/$', views.detail, name='athlete_detail'),
-    url(r'^athlete/new/', views.new, name='athlete_new'),
+    url(r'^$', views.athlete_index, name='index'),
+    url(r'^athlete/(?P<athleteId>\d+)/$', views.athlete_detail, name='athlete_detail'),
+    url(r'^athlete/new/', views.athlete_new, name='athlete_new'),
 )
