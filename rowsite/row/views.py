9a8080d55da31563475027f5771da2a6d33a32ae
= <eswalker@princeton.edu>
2014-03-29 15:20:06 -0400

    practice controller index, moved index.html

--- rowsite/row/views.py
+++ rowsite/row/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
-
-from row.models import Athlete, Weight
+from row.models import Athlete, Weight, Practice, Result
 
 # Lists athletes in a roster
 def athlete_index(request):
     athletes = Athlete.objects.all()
     context = {'athletes': athletes}
-    return render(request, 'row/index.html', context)
+    return render(request, 'row/athlete/index.html', context)
 
 # Shows athlete details for one athlete
 def athlete_detail(request, athleteId):
@@ -16,12 +14,15 @@ def athlete_detail(request, athleteId):
     weights = Weight.objects.get(athlete=athleteId)
     return render(request, 'row/athlete/details.html', {'athlete':athlete, 'weights':weights})
 
-
 # Creates a new athlete
 def athlete_new(request):
     return render(request, 'row/athlete/new.html')
 
+# Lists practices by date
+def practices_index(request):
+	practices = Practice.objects.all()
+	context = {'practices': practices}
+	return render(request, 'row/practice/index.html', context)
 
-	
 
 
