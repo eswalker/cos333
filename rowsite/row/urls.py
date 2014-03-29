df643967e629327272e8a3387f81097e2ed16573
= <eswalker@princeton.edu>
2014-03-29 15:55:31 -0400

    urls underscores

--- rowsite/row/urls.py
+++ rowsite/row/urls.py
@@ -4,8 +4,8 @@ from row import views
 urlpatterns = patterns('',
     url(r'^$', views.athlete_index, name='index'),
     url(r'^athletes/$', views.athlete_index, name='athlete_index'),
-    url(r'^athlete/(?P<athleteId>\d+)/$', views.athlete_detail, name='athlete_detail'),
+    url(r'^athlete/(?P<athlete_id>\d+)/$', views.athlete_detail, name='athlete_detail'),
     url(r'^athlete/new/', views.athlete_new, name='athlete_new'),
-    url(r'^practice/(?P<practiceId>\d+)/$', views.practice_detail, name='practice_detail'),
+    url(r'^practice/(?P<practice_id>\d+)/$', views.practice_detail, name='practice_detail'),
 
 )
