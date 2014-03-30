720771517c1c3f37b49048854763ae1d7ea5ce36
Brian Rosenfeld <bmr0802@gmail.com>
2014-03-29 21:55:36 -0400

    added label names to forms

--- rowsite/row/forms.py
+++ rowsite/row/forms.py
@@ -3,17 +3,17 @@ from datetime import datetime
 from row.models import Athlete, Weight, Practice, Result
 
 class AthleteForm(forms.ModelForm):
-    name = forms.CharField(max_length=50, help_text="Your full name.")
-    side = forms.CharField(max_length=20, help_text="Port, Starboard, Cox, Coach, or Other")
-    year = forms.CharField(max_length=20, help_text="Fr, So, Jr, or Sr")
-    height = forms.CharField(max_length=20, help_text="Your height in inches")
+    name = forms.CharField(max_length=50, help_text="Your full name.", label="name")
+    side = forms.CharField(max_length=20, help_text="Port, Starboard, Cox, Coach, or Other", label="side")
+    year = forms.CharField(max_length=20, help_text="Fr, So, Jr, or Sr", label="year")
+    height = forms.CharField(max_length=20, help_text="Your height in inches", label="height")
 
     class Meta:
         model = Athlete
 
 class PracticeForm(forms.ModelForm):
-	name = forms.CharField(max_length=20, help_text="What was the practice?")
-	datetime = forms.DateTimeField(initial=datetime.now(), help_text="When was the practice? (Ex. 3/29/14 8:30)")
+	name = forms.CharField(max_length=20, help_text="What was the practice?", label="name")
+	datetime = forms.DateTimeField(initial=datetime.now(), help_text="When was the practice? (Ex. 3/29/14 8:30)", label="datetime")
 
 	# An inline class to provide additional information on the form.
 	class Meta:
