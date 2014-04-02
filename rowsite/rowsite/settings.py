ccde082ae69d8cab3bbfe3c3bff5603a52289be4
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-01 21:02:29 -0400

    heroku commit

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -56,12 +56,10 @@ WSGI_APPLICATION = 'rowsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
