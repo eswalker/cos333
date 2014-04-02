e4c3856cc8e0b1ce5efb2f056d5c36b1229359ca
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-02 02:41:40 -0400

    potentially fixed database

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -59,6 +59,7 @@ WSGI_APPLICATION = 'rowsite.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] =  dj_database_url.config()
 
 """
