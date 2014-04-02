f238e59a77740d1c82e7d7029ee42fece0e7739a
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-02 01:06:02 -0400

    heroku with sqlite

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -56,10 +56,12 @@ WSGI_APPLICATION = 'rowsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-import dj_database_url
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config()
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
