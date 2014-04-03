8417e18836f2864ac42b90594450605b59f29f5f
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-03 18:09:47 -0400

    configured wsgi for local and production

--- rowsite/rowsite/wsgi.py
+++ rowsite/rowsite/wsgi.py
@@ -6,9 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rowsite.settings")
-
 from django.core.wsgi import get_wsgi_application
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rowsite.settings")
 application = get_wsgi_application()
+
+"""# For Production
+from dj_static import Cling
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rowsite.prod")
+application = Cling(get_wsgi_application()) """
