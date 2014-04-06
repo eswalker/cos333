b6b767a30a977131996cf60a320f89be22da28f1
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-05 22:19:42 -0400

    modified static root

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -92,5 +92,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_URL = '/row/static/'
-STATIC_ROOT = '/row'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/row/'
