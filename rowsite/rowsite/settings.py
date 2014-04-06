573daa4e6168087d63ce06522ad3fb73dc2eab67
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-05 20:49:56 -0400

    for heroku

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'c46=#x6e6tzhtu5dnz2knd_t_183qj!yf$ehg+my7iexs3r)px'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
