b8659ecfa23f62abdf232c7e2c67cb1416a1e6ee
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-05 22:32:41 -0400

    empty static root

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -92,9 +92,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
-STATIC_ROOT = '/row/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/row/static'),
+    os.path.join(BASE_DIR, '../rowsite/row/static'),
 )
