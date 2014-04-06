f5222725c3539eb3423e427a6949be8d17b0d534
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-05 22:46:45 -0400

    static file finders

--- rowsite/rowsite/settings.py
+++ rowsite/rowsite/settings.py
@@ -98,3 +98,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../rowsite/row/static'),
 )
+
+STATICFILES_FINDERS = ( 
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
