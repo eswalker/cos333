64b97992fc7c28bb92bfa44d45fed907a7605eeb
= <eswalker@princeton.edu>
2014-04-16 22:22:16 -0400

    erg layout

--- rowsite/row/static/row/js/erg.js
+++ rowsite/row/static/row/js/erg.js
@@ -20,13 +20,41 @@ $( "#sortable" ).sortable({
       helper: "clone",
       revert: "invalid"
     });
-    $( "ul, span" ).disableSelection();
+    $( "ul, span, ._ergs, ._row, ._erg, div" ).disableSelection();
 
 });
 
 
-
-
-    $( "#sortable0, #sortable1, #sortable2" ).sortable({
+    $( ".connectedSortable" ).sortable({
       connectWith: ".connectedSortable"
     }).disableSelection();
+
+num_rows = 3;
+
+function add_row(){
+	
+	$(this).toggle();
+
+	$('#content').append('<br><div class="_row"><p>Row ' + num_rows + '</p><ul id="sortable2" class="connectedSortable _ergs"></ul></div><br><button class="btn btn-primary _add_row">Add Row</button>');
+	num_rows += 1;
+
+	$( "#sortable" ).sortable({
+      revert: true
+    });
+    $( "#draggable" ).draggable({
+      connectToSortable: "#sortable",
+      helper: "clone",
+      revert: "invalid"
+    });
+    $( "ul, span, ._ergs, ._row, ._erg, div" ).disableSelection();
+
+ $( ".connectedSortable" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+
+ 	$('._add_row').click(add_row);
+
+}
+
+
+$('._add_row').click(add_row);
