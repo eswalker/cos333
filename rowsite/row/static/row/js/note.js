9a96f83a9ffde77de7a9fc9805e4a5a9d01210e5
Brian Rosenfeld <bmr0802@gmail.com>
2014-04-18 18:06:37 -0400

    + and - symbol for expanding notes

--- rowsite/row/static/row/js/note.js
+++ rowsite/row/static/row/js/note.js
@@ -1,5 +1,12 @@
 $( "tr.note_head").click(
 	function() {
 		$(this).next().toggle();
+		var symbol = $(this).find("b.note_subject_symbol");
+		if ($(symbol).text() == '+') {
+			$(symbol).text('—');
+		} else {
+			$(symbol).text('+');
+		}
+
 	}
 );
